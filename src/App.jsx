6fefaa1216b903d4244f3a69b18bdd79dad9ffac
YurkoKuksa <145128983+YurkoKuksa@users.noreply.github.com>
2024-08-23 22:24:16 +0200

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,22 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
-import React from "react";
+import Layout from "./components/Layout/Layout";
+import Home from "./pages/Home/Home";
+import Sources from "./pages/Sources/Sources";
+import Contacts from "./pages/Contacts/Contacts";
 
 function App() {
   return (
-    // <>
-    //   <p>Приваіт</p>
-    // </>
-
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>ЩО ТИ </p>
-      </header>
-    </div>
+    <>
+      <p>HUHUHUHUHUHUHU</p>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/sources" element={<Sources />} />
+          <Route path="/contacts" element={<Contacts />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-import App from "./App";
-// import reportWebVitals from './reportWebVitals';
 
-import { GlobalStyle } from "./styles/GlobalStyle";
+import App from "./App";
+// import { GlobalStyle } from "./styles/GlobalStyle";
+// import { ThemeProvider } from "styled-components";
+// import { theme } from "./styles/theme";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    {/* <React.StrictMode> */}
-    <App />
-    <GlobalStyle />
-    {/* </React.StrictMode> */}
+    {/* <BrowserRouter basename="/fullsdev">
+      <ThemeProvider theme={theme}>
+        <App />
+        <GlobalStyle />
+      </ThemeProvider>
+    </BrowserRouter> */}
+
+    <BrowserRouter basename="/engl">
+      <App />
+    </BrowserRouter>
   </>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
